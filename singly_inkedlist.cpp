cb57d6ab0ad50cdd4bc1a7a7c8ad78391461f483
rafiammar014 <rafiammar014@gmail.com>
2026-04-24 15:39:20 +0700

    Menambahkan implementasi fungsi Search pada LinkedList

--- singly_inkedlist.cpp
+++ singly_inkedlist.cpp
@@ -66,9 +66,16 @@ public
 
     bool Search(int nim, Node **previous, Node **current)
 {
+    *previous = START:
+    *current = START;
 
+    while ((*current != NULL) && (nim != (*current)->noMhs))
+    {
+    *previous = *current;
+    currrent = (*current)->next;
+    }
 
-
+    return (*current != NULL);
 }
 
 
